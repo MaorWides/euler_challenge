aaee707dfb67fd0a887c93dcff46dd08adba0ccf
MaorWides <113823711+MaorWides@users.noreply.github.com>
2023-03-12 15:09:14 +0200

    Update Main.java

--- src/main/java/i0/sealights/octolazersJava/Main.java
+++ src/main/java/i0/sealights/octolazersJava/Main.java
@@ -1,6 +1,7 @@
 package i0.sealights.octolazersJava;
 import org.parboiled.common.Tuple2;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class Main {
@@ -9,9 +10,9 @@ public class Main {
      */
     public static final Random rand = new Random();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String[][] grid = Helpers.rndGrid(12, 12, 6);
-        Helpers.Update(grid);
+        Helpers.update(grid);
         int pos = rand.nextInt(grid.length) + 1;
         LazerData.Direction direct = LazerData.nextRandom();
         Tuple2<Integer, LazerData.Direction> shot = Helpers.Shoot(grid, pos, direct);
